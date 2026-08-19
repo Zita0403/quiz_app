51a5df29c11cfb2a807ba2b101781af527df1ddf
Zita0403 <zitalukacs7@gmail.com>
2026-08-19 12:10:29 +0200

    chore: ensure views directory is correctly set for EJS rendering

--- app/index.js
+++ app/index.js
@@ -28,6 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 let totalScore = 0;
 let askedQuestions = [];
